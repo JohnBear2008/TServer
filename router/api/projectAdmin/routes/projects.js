183157f61c3667e824a2052bd822a6df8d04157c
John Bear <johnbear2008@qq.com>
2021-06-23 15:36:56 +0800

    Update projects.js

--- router/api/projectAdmin/routes/projects.js
+++ router/api/projectAdmin/routes/projects.js
@@ -1,9 +1,21 @@
 const Router = require('koa-router');
 const router = new Router()
+const nedb=require('../../../../database/nedb')
 
 
 //接口测试
 router.get('/', async (ctx, next) => {
+    next()
+    ctx.response.body = {
+        data: 'route projectAdmin/projects'
+    };
+});
+
+//接口测试
+router.post('/insert', async (ctx, next) => {
+    console.log('insert', ctx.request.body);
+
+    nedb.insertDB({name:'test',data:{a:1}})
     next()
     ctx.response.body = {
         data: 'route projectAdmin/projects'
