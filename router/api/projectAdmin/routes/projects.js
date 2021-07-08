1b70b46ced6ed914443847b9184402e244ee8d31
John Bear <johnbear2008@qq.com>
2021-07-08 14:28:48 +0800

    Update projects.js

--- router/api/projectAdmin/routes/projects.js
+++ router/api/projectAdmin/routes/projects.js
@@ -35,38 +35,58 @@ router.post('/insert', async (ctx, next) => {
 //接口测试
 router.post('/save', async (ctx, next) => {
     console.log('save', ctx.request.body);
-    let filter = {
-        id: ctx.request.body.id
-    }
-
     let isExistRS = await nedb.isExist({
         name: 'projectAdmin_projects',
-        filter: filter
+        filter: {
+            _id: ctx.request.body._id
+        }
     })
+    console.log('isExistRS1', isExistRS);
 
-    console.log('isExistRS', isExistRS);
     let rs = {};
-    if (isExistRS) {
-        let updateRs = await nedb.updateOneDB({
-            name: 'projectAdmin_projects',
-            filter: filter,
-            data: ctx.request.body
-        })
-        rs = updateRs ? {
-            result: 'success'
-        } : {
-            result: 'fail'
-        }
-    } else {
-        let insertRs = await nedb.insertDB({
-            name: 'projectAdmin_projects',
-            data: ctx.request.body
-        })
-        rs = insertRs ? {
-            result: 'success'
-        } : {
-            result: 'fail'
-        }
+    switch (true) {
+
+        case isExistRS:
+            let updateRs = await nedb.updateOneDB({
+                name: 'projectAdmin_projects',
+                filter: {
+                    _id: ctx.request.body._id
+                },
+                data: ctx.request.body
+            })
+            rs = updateRs ? {
+                result: 'success'
+            } : {
+                result: 'fail'
+            }
+            break;
+
+        default:
+            let isDuplicate = await nedb.isExist({
+                name: 'projectAdmin_projects',
+                filter: {
+                    projectId: ctx.request.body.projectId
+                }
+            })
+            console.log('isDuplicate2', isDuplicate);
+
+            if (isDuplicate) {
+                rs = {
+                    result: 'duplicate'
+                }
+            } else {
+                let insertRs = await nedb.insertDB({
+                    name: 'projectAdmin_projects',
+                    data: ctx.request.body
+                })
+                rs = insertRs ? {
+                    result: 'success'
+                } : {
+                    result: 'fail'
+                }
+            }
+
+            break;
     }
     next()
     ctx.response.body = rs
@@ -78,7 +98,7 @@ router.post('/delete', async (ctx, next) => {
     console.log('delete', ctx.request.body);
     let idArr = ctx.request.body.idArr
     let filter = {
-        id: {
+        _id: {
             $in: idArr
         }
     }
